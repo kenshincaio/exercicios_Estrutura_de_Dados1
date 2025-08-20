8eb14fa60f7a89b07cf82ae4cb79a3ddd636d815
Caio Yamada <yamada.caiokenshin@gmail.com>
2025-08-20 15:19:05 -0300

    Update resposta01.c

--- resposta01.c
+++ resposta01.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int main() {
+    
     int array1[50], array2[25], i;
 
     printf("Insira 50 valores inteiros para o primeiro array\n");
@@ -9,10 +10,11 @@ int main() {
         scanf("%i",&array1[i]);
     }
     for(i = 0; i < 25; i++){
-            array2[i] = array1[2 * i] * array1[2 * i + 1];  
+        array2[i] = array1[2 * i] * array1[2 * i + 1];  
     }
     for(i = 0; i < 25; i++){
         printf("O valor do array2[%i] = %i\n", i, array2[i]);
     }
     return 0;
+
 }
